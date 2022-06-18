7ea24cadfccfcd002b055a24cd3ede30a23fa7e5
Amkam <amaduskamara@gmail.com>
2022-06-18 13:33:00 +0000

    Add create goal functionality

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -17,7 +17,12 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  res.status(201).json({ message: "POST Goal" });
+
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(201).json(goal);
 });
 
 // @desc   Update a goal
