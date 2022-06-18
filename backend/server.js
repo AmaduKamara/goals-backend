4c52667661612946c9aaa1e3e0043d8dcc63e084
Amkam <amaduskamara@gmail.com>
2022-06-18 11:22:37 +0000

    Setup basic express server and a route to get all goals

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+
+// Init express app
+const app = express();
+
+// Routes
+app.get("/api/goals", (req, res) => {
+  res.status(200).json({ message: "GET Goals" });
+});
+
+// Run express server
+app.listen(port, () => console.log(`Server started on port: ${port}`));
