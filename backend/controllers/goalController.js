e7db64a457e77dfd19f7053fa48bc89648cedc76
Amkam <amaduskamara@gmail.com>
2022-06-18 12:22:40 +0000

    Add express error handler and express asyn handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,31 +1,34 @@
+const asyncHandler = require("express-async-handler");
 // @desc   Get goals
 // @route  GET /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "GET Goals" });
-};
+});
 
 // @desc   Create goal
 // @route  POST /api/goals
 // @access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
   res.status(201).json({ message: "POST Goal" });
-};
+});
 
 // @desc   Update a goal
 // @route  PUT /api/goals/:id
 // @access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `UPDATE a Goal ${req.params.id}` });
-};
+});
 
 // @desc   Delete goal
 // @route  DELETE /api/goals/:id
 // @access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `DELETE a Goal ${req.params.id}` });
-};
-
-
+});
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const port = process.env.PORT || 5000;
 
 // Init express app
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
 // Routes
 app.use("/api/goals", require("./routes/goalRoutes"));
+
+// Error Middleware
+app.use(errorHandler);
 
 // Run express server
 app.listen(port, () => console.log(`Server started on port: ${port}`));
